5ff8a69aef53d10d3fda55de18a953ee74dcc116
kbo4sho88 <kevinbolander@gmail.com>
2019-07-15 18:06:51 -0500

    clicking hookpoints

---
+++ Assets/ClickConnect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickConnect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distance = 100;
+            if(Physics.Raycast(ray, out hit, distance))
+            {
+                if (hit.rigidbody.tag == "Hookable")
+                {
+                    Debug.Log("Hookable clicked");
+                    // Connect the joint to the new hookpoint
+                }
+                   
+                
+
+            }
+        }
+    }
+}
