ac82e46475cf74bbaa33481488b9b6e95865fdb0
kbo4sho88 <kevinbolander@gmail.com>
2019-07-17 09:04:24 -0500

    points genereator

---
+++ Assets/Scripts/HookPointGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookPointGenerator : MonoBehaviour
+{
+    public float startCordinates;
+    public float endCordinates;
+    // public List<GameObject> hookPoints;
+     // WIll look at children in the UI instead, means the delay needs to be configured in the UI as well.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
